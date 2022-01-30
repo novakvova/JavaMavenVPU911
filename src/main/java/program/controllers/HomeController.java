772adb742040043449f60cdf7674fb761db3b9a4
novakvova <novakvova@gmail.com>
2022-01-30 12:39:56 +0200

    Add mvc

--- src/main/java/program/controllers/HomeController.java
+++ src/main/java/program/controllers/HomeController.java
@@ -1,13 +1,18 @@
 package program.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import program.repositories.RoleRepository;
 
-@RestController
+@Controller
+@RequiredArgsConstructor
 public class HomeController {
-
-    @GetMapping("/")
-    public String index() {
-        return "Hello Spring Boot";
+    private final RoleRepository roleRepository;
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("roles", roleRepository.findAll());
+        return "main/index";
     }
 }
